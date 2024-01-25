cd7028d55814bd7c04eae4787b9b6b7e123c8e1c
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-01-25 16:08:37 +0200

    3

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,13 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if ()
+  });
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+});
